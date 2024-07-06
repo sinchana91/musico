7ab1addad4c17545bb6a8e94c007e90721143503
sinchana c poojary <sinchanac91@gmail.com>
2024-07-06 23:20:19 +0530

    server and auth setup was added

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,16 @@
 import jwt from 'jsonwebtoken';
+
+export function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).json({ message: 'No token, authorization denied' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Token is not valid' });
+    }
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import {userRoute} from '/routes/userRoutes.js';
+import {playlistRoute} from '/routes/playlistRoutes.js';
+import {songRoute} from '/routes/songRoutes.js';
+import {authRoute} from '/routes/authRoutes.js';
+
+dotenv.config();
+const app=express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('API is running');
+});
+
+
+app.use('/api/users',userRoute);
+app.use('/api/auth',authRoute);
+app.use('/api/playlists',playlistRoute);
+app.use('/api/songs',songRoute);
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,console.log(`Server running in  port ${PORT}`));
