7a8eb20dd3db068d16c46aaffb2a756c010aeabd
sinchana c poojary <sinchanac91@gmail.com>
2024-07-08 20:22:34 +0530

    Refactor songController to fetch song data from MusicBrainz API

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,17 +1,36 @@
-import prisma from "../config/db";
+import axios from 'axios';
+
+const fetchSongFromAPI = async (id) => {
+    try{
+        const response=await axios.get(`https://musicbrainz.org/ws/2/recording/${id}`,{headers: {Accept: 'application/json'}});
+        return response.data;
+    }catch(error){
+        console.error(`error fetching song data from ID ${id}:`,error);
+        throw error;
+        
+    }
+}
+
+const fetchSongsFromAPI=async(query){
+    try{
+        const response = await axios.get(`https://musicbrainz.org/ws/2/recording`, {
+            params: { query, limit: 10 },
+            headers: { Accept: 'application/json' }
+        });
+        return response.data.recordings;
+
+    }catch(error){
+        console.error(`Error fetching songs data for query ${query}:`, error);
+        throw error;
+
+    }
+}
 
 const createSong = async (req, res) => {
-    const {title,artisit,genre,duration}=req.body;
+    const {id}=req.body;
     try{
-        const song=await prisma.song.create({
-            data:{
-                title,
-                artisit,
-                genre,
-                duration
-            }
-        });
-        res.status(200).json({song});
+        const song=await fetchSongFromAPI(id)
+        res.status(200).json({song})
     }catch(error){
         console.error(error);
         res.status(500).json({message:error.message});
@@ -20,29 +39,23 @@ const createSong = async (req, res) => {
 
 const getAllSongs = async (req, res) => {
     try{
-        const songs=await prisma.song.findMany();
-        res.status(200).json({songs});
+        const songs = await fetchSongsFromAPI(''); // Fetch top songs or any default query
+        res.status(200).json({ songs });
     }catch(error){
         console.error(error);
         res.status(500).json({message:error.message});
     }
 }
 
-const seachSong = async (req, res) => {
+const searchSong = async (req, res) => {
     const {title}=req.query;
     try{
-        const songs=await prisma.song.findMany({
-            where:{
-                title:{
-                    contains:title
-                }
-            }
-        });
-        res.status(200).json({songs});
+        const songs = await fetchSongsFromAPI(title);
+        res.status(200).json({ songs });
     }catch(error){
         console.error(error);
         res.status(500).json({message:error.message});
     }
 }
 
-export {createSong,getAllSongs,seachSong};
+export {createSong,getAllSongs,searchSong};
